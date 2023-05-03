4acce4babb29436225eddeea706e73242dcac7d3
Loïc <luigi@cavinato.fr>
2023-05-03 22:16:16 +0200

    passage solid

---
+++ src/routes/index.jsx
@@ -0,0 +1,35 @@
+import { Title } from "solid-start";
+import Counter from "~/components/Counter";
+import "../stylus/global.styl"
+export default function Home() {
+  return (
+    <main style="width:44.7%;margin:auto;">
+      <h1>Hello world!</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti doloribus explicabo doloremque facilis facere dicta totam molestiae nostrum, atque quod, tenetur molestias reiciendis iusto nemo dolores numquam harum id. Iure!</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti doloribus explicabo doloremque facilis facere dicta totam molestiae nostrum, atque quod, tenetur molestias reiciendis iusto nemo dolores numquam harum id. Iure!</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti doloribus explicabo doloremque facilis facere dicta totam molestiae nostrum, atque quod, tenetur molestias reiciendis iusto nemo dolores numquam harum id. Iure!</p>
+      <ul>
+        <li>Bla bla bla</li>
+        <li>Bla bla bla</li>
+        <li>Bla bla bla</li>
+        <li>Bla bla bla</li>
+      </ul>
+      <h2>Hello world!</h2>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti doloribus explicabo doloremque facilis facere dicta totam molestiae nostrum, atque quod, tenetur molestias reiciendis iusto nemo dolores numquam harum id. Iure!</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti doloribus explicabo doloremque facilis facere dicta totam molestiae nostrum, atque quod, tenetur molestias reiciendis iusto nemo dolores numquam harum id. Iure!</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti doloribus explicabo doloremque facilis facere dicta totam molestiae nostrum, atque quod, tenetur molestias reiciendis iusto nemo dolores numquam harum id. Iure!</p>
+      <h3>Hello world!</h3>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti doloribus explicabo doloremque facilis facere dicta totam molestiae nostrum, atque quod, tenetur molestias reiciendis iusto nemo dolores numquam harum id. Iure!</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti doloribus explicabo doloremque facilis facere dicta totam molestiae nostrum, atque quod, tenetur molestias reiciendis iusto nemo dolores numquam harum id. Iure!</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti doloribus explicabo doloremque facilis facere dicta totam molestiae nostrum, atque quod, tenetur molestias reiciendis iusto nemo dolores numquam harum id. Iure!</p>
+      <h4>Hello world!</h4>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti doloribus explicabo doloremque facilis facere dicta totam molestiae nostrum, atque quod, tenetur molestias reiciendis iusto nemo dolores numquam harum id. Iure!</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti doloribus explicabo doloremque facilis facere dicta totam molestiae nostrum, atque quod, tenetur molestias reiciendis iusto nemo dolores numquam harum id. Iure!</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti doloribus explicabo doloremque facilis facere dicta totam molestiae nostrum, atque quod, tenetur molestias reiciendis iusto nemo dolores numquam harum id. Iure!</p>
+      <h5>Hello world!</h5>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti doloribus explicabo doloremque facilis facere dicta totam molestiae nostrum, atque quod, tenetur molestias reiciendis iusto nemo dolores numquam harum id. Iure!</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti doloribus explicabo doloremque facilis facere dicta totam molestiae nostrum, atque quod, tenetur molestias reiciendis iusto nemo dolores numquam harum id. Iure!</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti doloribus explicabo doloremque facilis facere dicta totam molestiae nostrum, atque quod, tenetur molestias reiciendis iusto nemo dolores numquam harum id. Iure!</p>
+    </main>
+  );
+}
